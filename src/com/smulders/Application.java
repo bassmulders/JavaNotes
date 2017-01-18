db93d1a59df02bdb522728477f5be925f1e094cd
Bas <bas.smulders@gmail.com>
2017-01-18 10:56:33 +0100

    Add LinkedList class

--- src/com/smulders/Application.java
+++ src/com/smulders/Application.java
@@ -1,245 +1,102 @@
 package com.smulders;
 import com.smulders.Node;
+import com.smulders.LinkedList;
+//import java.util.logging.Logger;
 
 public class Application {
 	
 	static RandomStringGenerator rsg;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		rsg = new RandomStringGenerator();
-		Node ll;
-		ll = makeLinkedList(10);
+		LinkedList ll;// = new LinkedList(head);
+		try {
+			ll = makeLinkedList(17);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return;
+		}
+		
 		System.out.println("Unsorted linked list:");
-		printLinkedList(ll);
-		System.out.println("\nReversed linked list:");
-		printLinkedListReversed(ll);
-		ll = makeSortedLinkedList(10);
-		System.out.println("\nSorted linked list:");
-		printLinkedList(ll);
-		ll = insertIntoSortedLinkedList(ll, "k");
-		System.out.println("\nInserted 'k' into sorted linked list:");
-		printLinkedList(ll);
-		ll = deleteFromLinkedList(ll, "k");
-		System.out.println("\nDeleted 'k' from sorted linked list:");
-		printLinkedList(ll);
-		ll = appendToLinkedList(ll, "k");
-		System.out.println("\nAppended 'k' to sorted linked list:");
-		printLinkedList(ll);
-		ll = deleteFromLinkedList(ll, "k");
-		System.out.println("\nDeleted 'k' from sorted linked list:");
-		printLinkedList(ll);
-		
-	}
-	
-	private static Node makeSortedLinkedList(int itemCount) {
-		Node head = null;
-		for (int i = 0; i < itemCount; i++) {
-			head = insertIntoSortedLinkedList(head, rsg.nextString());
-		}
-		return head;
-	}
-	
-	private static Node makeLinkedList(int itemCount) {
-		Node head = null;
-		if (itemCount > 0) {
-			head = new Node(rsg.nextString());
-			Node runner = head;
-			for (int i = 1; i < itemCount; i++) {
-				runner.next = new Node(rsg.nextString());
-				runner = runner.next;
-			}
-		}
-		return head;
-	}
-	
-	private static void printLinkedList(Node node) {
-		while (node != null) {
-			System.out.println(node.item);
-			node = node.next;
-		}
-	}
+		ll.print();
 
-	private static void printLinkedListReversed(Node node) {
-		if (node != null) {
-			printLinkedListReversed(node.next);
-			System.out.println(node.item);
-		}
-	}
-	
-	private static Node appendToLinkedList(Node head, String newItem) {
-		if (head == null) {
-			return new Node(newItem);
-		}
-		Node runner = head;
-		while (runner.next != null) {
-			runner = runner.next;
-		}
-		runner.next = new Node(newItem);
-		return head;
-	}
-	
-	private static Node insertIntoSortedLinkedList(Node head, String newItem) {
-		Node newNode, previous;
-		newNode = new Node(newItem);
+		System.out.println("\nSorted linked list:");
+		ll.mergeSort();
+		ll.print();
 		
-		if (head == null) {
-			return newNode;
-		}
+		System.out.println("\nReversed linked list:");
+		ll.printReversed();
 		
-		previous = findNodeWithBiggestSmallerValue(head, newItem);
-		if (previous == null) {
-			newNode.next = head;
-			return newNode;
-		} else {
-			newNode.next = previous.next;
-			previous.next = newNode;
-			return head;
-		}
+		System.out.println("\nInserted 'k' into sorted linked list:");
+		ll.insert("k");
+		ll.print();
+
+		System.out.println("\nDeleted 'k' from sorted linked list:");
+		ll.delete("k");
+		ll.print();
+
+		System.out.println("\nAppended '<appended string>' to sorted linked list:");
+		ll.append("<appended string>");
+		ll.print();
+
+		System.out.println("\nDeleted '<appended string>' from sorted linked list:");
+		ll.delete("<appended string>");
+		ll.print();
+		
 	}
 	
-	private static Node deleteFromLinkedList(Node head, String deleteItem) {
-		Node runner = head, previous = null;
-		while (runner != null) {
-			if (runner.item.equals(deleteItem)) {
-				if (previous == null) {
-					head = runner.next;
-				} else {
-					previous.next = runner.next;
-				}
-				runner = null;
-				return head;
-			}
-			previous = runner;
-			runner = runner.next;
+	private static LinkedList makeLinkedList(int itemCount) throws IllegalArgumentException {
+		if (itemCount < 0) throw new IllegalArgumentException("LinkedList cannot have negative number of elements.");
+		LinkedList ll = new LinkedList();
+		for (int i = 1; i <= itemCount; i++) {
+			ll.append(rsg.nextString());;
 		}
-		return head;
+		return ll;
 	}
 	
-	private static Node findNodeWithBiggestSmallerValue(Node node, String searchItem) {
-		// If the current node is null, or 
-		// the item value of the current node is larger than or equal to searchItem, then
-		// return null. There is no node in the list with a smaller value than searchItem.
-		if (node == null || node.item.compareTo(searchItem) >= 0) {
-			return null;
-		}
-		
-		// If we arrive here and there is only one node in the list, the item value
-		// of this node must be smaller than searchItem.
-		if (node.next == null) {
-			return node;
-		}
-		
-		Node runner = node;
-		while (runner.next != null) {
-			if (runner.next.item.compareTo(searchItem) >= 0) {
-				return runner;
-			}
-			runner = runner.next;
-		}
-		return runner;
-	}
-	
-//	private static Node SortLinkedList(Node node) {
-//		Node p, q, e, tail, oldhead;
-//		int insize, nmerges, psize, qsize, i;
-//		
-//		if (node == null) return null;
-//		
-//		insize=1;
+//	private static void printLinkedList(Node node) {
+//		while (node != null) {
+//			System.out.println(node.item);
+//			node = node.next;
+//		}
+//	}
+//
+//	private static void printLinkedListReversed(Node node) {
+//		if (node != null) {
+//			printLinkedListReversed(node.next);
+//			System.out.println(node.item);
+//		}
 //	}
 //	
-//	element *listsort(element *list, int is_circular, int is_double) {
-//	    element *p, *q, *e, *tail, *oldhead;
-//	    int insize, nmerges, psize, qsize, i;
+//	private static Node appendToLinkedList(Node head, String newItem) {
+//		if (head == null) {
+//			return new Node(newItem);
+//		}
+//		Node runner = head;
+//		while (runner.next != null) {
+//			runner = runner.next;
+//		}
+//		runner.next = new Node(newItem);
+//		return head;
+//	}
 //
-//	    /*
-//	     * Silly special case: if `list' was passed in as NULL, return
-//	     * NULL immediately.
-//	     */
-//	    if (!list)
-//		return NULL;
-//
-//	    insize = 1;
-//
-//	    while (1) {
-//	        p = list;
-//		oldhead = list;		       /* only used for circular linkage */
-//	        list = NULL;
-//	        tail = NULL;
-//
-//	        nmerges = 0;  /* count number of merges we do in this pass */
-//
-//	        while (p) {
-//	            nmerges++;  /* there exists a merge to be done */
-//	            /* step `insize' places along from p */
-//	            q = p;
-//	            psize = 0;
-//	            for (i = 0; i < insize; i++) {
-//	                psize++;
-//			if (is_circular)
-//			    q = (q->next == oldhead ? NULL : q->next);
-//			else
-//			    q = q->next;
-//	                if (!q) break;
-//	            }
-//
-//	            /* if q hasn't fallen off end, we have two lists to merge */
-//	            qsize = insize;
-//
-//	            /* now we have two lists; merge them */
-//	            while (psize > 0 || (qsize > 0 && q)) {
-//
-//	                /* decide whether next element of merge comes from p or q */
-//	                if (psize == 0) {
-//			    /* p is empty; e must come from q. */
-//			    e = q; q = q->next; qsize--;
-//			    if (is_circular && q == oldhead) q = NULL;
-//			} else if (qsize == 0 || !q) {
-//			    /* q is empty; e must come from p. */
-//			    e = p; p = p->next; psize--;
-//			    if (is_circular && p == oldhead) p = NULL;
-//			} else if (cmp(p,q) <= 0) {
-//			    /* First element of p is lower (or same);
-//			     * e must come from p. */
-//			    e = p; p = p->next; psize--;
-//			    if (is_circular && p == oldhead) p = NULL;
-//			} else {
-//			    /* First element of q is lower; e must come from q. */
-//			    e = q; q = q->next; qsize--;
-//			    if (is_circular && q == oldhead) q = NULL;
+//	private static Node deleteFromLinkedList(Node head, String deleteItem) {
+//		Node runner = head, previous = null;
+//		while (runner != null) {
+//			if (runner.item.equals(deleteItem)) {
+//				if (previous == null) {
+//					head = runner.next;
+//				} else {
+//					previous.next = runner.next;
+//				}
+//				runner = null;
+//				return head;
 //			}
-//
-//	                /* add the next element to the merged list */
-//			if (tail) {
-//			    tail->next = e;
-//			} else {
-//			    list = e;
-//			}
-//			if (is_double) {
-//			    /* Maintain reverse pointers in a doubly linked list. */
-//			    e->prev = tail;
-//			}
-//			tail = e;
-//	            }
-//
-//	            /* now p has stepped `insize' places along, and q has too */
-//	            p = q;
-//	        }
-//		if (is_circular) {
-//		    tail->next = list;
-//		    if (is_double)
-//			list->prev = tail;
-//		} else
-//		    tail->next = NULL;
-//
-//	        /* If we have done only one merge, we're finished. */
-//	        if (nmerges <= 1)   /* allow for nmerges==0, the empty list case */
-//	            return list;
-//
-//	        /* Otherwise repeat, merging lists twice the size */
-//	        insize *= 2;
-//	    }
+//			previous = runner;
+//			runner = runner.next;
+//		}
+//		return head;
 //	}
 
 }
---
+++ src/com/smulders/LinkedList.java
@@ -0,0 +1,159 @@
+package com.smulders;
+
+public class LinkedList {
+	
+	private Node list;
+	
+	LinkedList() {
+		this.list = null;
+	}
+
+	LinkedList(Node head) {
+		this.list = head;
+	}
+
+	public void append(String newItem) {
+		if (this.list == null) {
+			this.list = new Node(newItem);
+		} else {
+			Node node = this.list;
+			while (node.next != null) {
+				node = node.next;
+			}
+			node.next = new Node(newItem);
+		}
+	}
+	
+	public void delete(String deleteItem) {
+		Node runner = this.list, previous = null;
+		while (runner != null) {
+			if (runner.item.equals(deleteItem)) {
+				if (previous == null) {
+					this.list = runner.next;
+				} else {
+					previous.next = runner.next;
+				}
+				runner = null;
+				return;
+			}
+			previous = runner;
+			runner = runner.next;
+		}
+	}
+	
+	public void insert(String newItem) {
+		Node newNode, previous;
+		newNode = new Node(newItem);
+		
+		if (this.list == null) {
+			this.list = newNode;
+		}
+		
+		previous = findNodeWithBiggestSmallerValue(this.list, newItem);
+		if (previous == null) {
+			newNode.next = this.list;
+			this.list = newNode;
+		} else {
+			newNode.next = previous.next;
+			previous.next = newNode;
+		}
+	}
+
+	public int getLength() {
+		int count = 0;
+		Node node = this.list;
+		while (node != null) {
+			count++;
+			node = node.next;
+		}
+		return count;
+	}
+	
+	private Node findNodeWithBiggestSmallerValue(Node node, String searchItem) {
+		// If the current node is null, or 
+		// the item value of the current node is larger than or equal to searchItem, then
+		// return null. There is no node in the list with a smaller value than searchItem.
+		if (node == null || node.item.compareTo(searchItem) >= 0) {
+			return null;
+		}
+		
+		// If we arrive here and there is only one node in the list, the item value
+		// of this node must be smaller than searchItem.
+		if (node.next == null) {
+			return node;
+		}
+		
+		Node runner = node;
+		while (runner.next != null) {
+			if (runner.next.item.compareTo(searchItem) >= 0) {
+				return runner;
+			}
+			runner = runner.next;
+		}
+		return runner;
+	}
+	
+	/*
+	 * mergeList and mergeSort based on //http://algorithms.tutorialhorizon.com/merge-sort-in-a-linked-list/
+	 */
+	private Node mergeList(Node a, Node b) {
+		Node result = null;
+		if (a == null)
+			return b;
+		if (b == null)
+			return a;
+		if (a.item.compareTo(b.item) > 0) {
+			result = b;
+			result.next = mergeList(a, b.next);
+		} else {
+			result = a;
+			result.next = mergeList(a.next, b);
+		}
+		return result;
+	}
+
+	public void mergeSort() {
+		this.list = mergeSort(this.list);
+	}
+
+	private Node mergeSort(Node a) {
+		Node oldHead = a;
+		// find the length of the linkedlist
+		int mid = this.getLength() / 2;
+		if (a.next == null)
+			return a;
+		// set one pointer to the beginning of the list and another at the next
+		// element after mid
+		while (mid - 1 > 0) {
+			oldHead = oldHead.next;
+			mid--;
+		}
+		Node newHead = oldHead.next;// make newHead points to the beginning of
+									// the second half of the list
+		oldHead.next = null;// break the list
+		oldHead = a;// make one pointer points at the beginning of the first
+					// half of the list
+		Node t1 = mergeSort(oldHead);//make recursive calls 
+		Node t2 = mergeSort(newHead);
+		return mergeList(t1, t2); // merge the sorted lists
+	}
+	
+	public void print() {
+		Node node = this.list;
+		while (node != null) {
+			System.out.println(node.item);
+			node = node.next;
+		}
+	}
+	
+	private void printReversedRecursive(Node node) {
+		if (node != null) {
+			printReversedRecursive(node.next);
+			System.out.println(node.item);
+		}
+	}
+	
+	public void printReversed() {
+		printReversedRecursive(this.list);
+	}
+}
